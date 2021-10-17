4dcde8c132c28b39ceed34a88eb0b50ce47f43e0
NeskladAnya <belko.hanna.off@gmail.com>
2021-10-17 18:21:24 +0200

    Add basic card methods

---
+++ cards.rb
@@ -0,0 +1,30 @@
+class Cards
+  attr_reader :deck, :handled_cards
+
+  CARD_SUITS = %w(♠ ♥ ♣ ♦)
+  CARD_VALUES = %w(2 3 4 5 6 7 8 9 10 B D K A)
+
+  def initialize
+    @deck = []
+
+    CARD_VALUES.each do |value|
+      CARD_SUITS.each do |suit|
+        deck << value + suit
+      end
+    end
+  end
+
+  def handle_card(number)
+    cards = select_card(number)
+    cards.each { |card| @deck.delete(card) }
+  end
+
+  def select_card(number)
+    @handled_cards ||= []
+
+    cards = deck.sample(number)
+    cards.each { |card| handled_cards << card}
+
+    return cards
+  end
+end
