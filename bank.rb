4b79a755aa423535d024a4d147df133ba830766b
NeskladAnya <belko.hanna.off@gmail.com>
2021-10-17 21:57:12 +0200

    Add @bank to the class

--- bank.rb
+++ bank.rb
@@ -1,10 +1,25 @@
 class Bank
+  attr_accessor :bank
+
+  def initialize(amount)
+    @bank = amount
+  end
 
   def give_money(amount, *players)
-    players.each { |player| player.money += amount }
+    players.each do |player|
+      player.money += amount
+      @bank -= amount
+    end
+
+    return bank
   end
 
   def accept_money(amount, *players)
-    players.each { |player| player.money -= amount }
+    players.each do |player|
+      player.money -= amount
+      @bank += amount
+    end
+
+    return bank
   end
 end
