24e9f3d05ec36e69f57ce2609e4935213a89c179
NeskladAnya <belko.hanna.off@gmail.com>
2021-10-17 22:47:37 +0200

    Add skip_move methods

--- dealer.rb
+++ dealer.rb
@@ -1,9 +1,18 @@
 class Dealer
-  attr_accessor :money, :cards, :cards_weight
+  attr_accessor :money, :cards, :cards_weight, :move
   
   def initialize
     @money = 0
     @cards = []
     @cards_weight = 0
+    @move = false
+  end
+
+  def skip_move
+    raise "It's not your move" if move == false
+
+    move = false if cards_weight >= 17
+  rescue StandardError => e
+    puts e.message
   end
 end
--- player.rb
+++ player.rb
@@ -1,11 +1,22 @@
 class Player
+
+  
   attr_reader :name
-  attr_accessor :money, :cards, :cards_weight
+  attr_accessor :money, :cards, :cards_weight, :move
 
   def initialize(name)
     @name = name
     @money = 0
     @cards = []
     @cards_weight = 0
+    @move = false
+  end
+
+  def skip_move
+    raise "It's not your move" if move == false
+
+    move = false
+  rescue StandardError => e
+    puts e.message
   end
 end
