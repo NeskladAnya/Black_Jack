dfdc9cff05a487751ff510564b10af5f850b5bf0
NeskladAnya <belko.hanna.off@gmail.com>
2021-10-17 21:49:56 +0200

    Add cards_weight to player

--- player.rb
+++ player.rb
@@ -1,10 +1,11 @@
 class Player
   attr_reader :name
-  attr_accessor :money, :cards
+  attr_accessor :money, :cards, :cards_weight
 
   def initialize(name)
     @name = name
     @money = 0
     @cards = []
+    @cards_weight = 0
   end
 end
