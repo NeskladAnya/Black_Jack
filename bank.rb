5393fcbca5d76cf766ab99a4e50d8886a45c4413
NeskladAnya <belko.hanna.off@gmail.com>
2021-10-17 21:00:05 +0200

    Add bank accept_money method

--- bank.rb
+++ bank.rb
@@ -3,4 +3,7 @@ class Bank
   def give_money(amount, *players)
     players.each { |player| player.money += amount }
   end
+
+  def accept_money(amount, *players)
+    players.each { |player| player.money -= amount }
 end
