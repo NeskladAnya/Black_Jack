cd43e5df3fe2910bd11f68845b69da451a03a7e6
NeskladAnya <belko.hanna.off@gmail.com>
2021-10-18 19:23:12 +0200

    Remove skip_turn method

--- dealer.rb
+++ dealer.rb
@@ -1,18 +1,11 @@
 class Dealer
-  attr_accessor :money, :cards, :cards_weight, :move
+  attr_accessor :money, :cards, :cards_weight, :turn
   
   def initialize
     @money = 0
     @cards = []
     @cards_weight = 0
-    @move = false
+    @turn = false
   end
 
-  def skip_move
-    raise "It's not your move" if move == false
-
-    move = false if cards_weight >= 17
-  rescue StandardError => e
-    puts e.message
-  end
 end
--- player.rb
+++ player.rb
@@ -1,22 +1,13 @@
 class Player
-
-  
   attr_reader :name
-  attr_accessor :money, :cards, :cards_weight, :move
+  attr_accessor :money, :cards, :cards_weight, :turn
 
   def initialize(name)
     @name = name
     @money = 0
     @cards = []
     @cards_weight = 0
-    @move = false
+    @turn = false
   end
 
-  def skip_move
-    raise "It's not your move" if move == false
-
-    move = false
-  rescue StandardError => e
-    puts e.message
-  end
 end
